f1d9e85dc7bbd8817f32b1e21000e523bbc088b3
“KarxGonzo” <KarxGonzo@gmail.com>
2021-07-14 20:30:37 -0400

    Adding Libs

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
+var moment = require("moment"); // require
+moment().format();
+
 console.log("Hello!");
+
+console.log(moment().startOf("day").fromNow());
+console.log(moment().endOf("day").fromNow());
